bf89672aa52f3b52de08f0a573a4bcfe0ee49893
masa-s60 <g.1019cord.jhp@gmail.com>
2022-10-30 14:43:54 +0900

    vue.js機能追加

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+  export default {
+    name: 'HelloVie',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
